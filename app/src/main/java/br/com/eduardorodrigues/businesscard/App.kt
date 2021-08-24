5ec92ac4e2a1ad408372698056a554c5e72023e8
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-08-24 17:24:59 -0300

    Configuração do Room Database - Criação das classes AppDatabase, DAO, Repository e model; Injetado na classe App

--- app/src/main/java/br/com/eduardorodrigues/businesscard/App.kt
+++ app/src/main/java/br/com/eduardorodrigues/businesscard/App.kt
@@ -1,6 +1,10 @@
 package br.com.eduardorodrigues.businesscard
 
 import android.app.Application
+import br.com.eduardorodrigues.businesscard.data.AppDatabase
+import br.com.eduardorodrigues.businesscard.data.BusinessCardRepository
 
 class App: Application() {
+    val database by lazy{ AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
