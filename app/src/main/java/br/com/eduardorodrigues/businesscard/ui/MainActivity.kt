ab816fa3facc77838dc053bf73ff20e9bde897fb
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-08-24 18:47:33 -0300

    Configuração do Room Database - Criação de Adapter para listagem dos Cards na View

--- app/src/main/java/br/com/eduardorodrigues/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/eduardorodrigues/businesscard/ui/MainActivity.kt
@@ -3,14 +3,25 @@ package br.com.eduardorodrigues.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import br.com.eduardorodrigues.businesscard.App
 import br.com.eduardorodrigues.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -18,6 +29,12 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener{
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCards ->
+            adapter.submitList(businessCards)
         }
     }
 }
